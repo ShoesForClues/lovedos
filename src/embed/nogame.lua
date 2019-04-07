ed12192574745e8b071c9c1be6f461533c6b830c
Jason Lee <38192767+ShoesForClues@users.noreply.github.com>
2019-04-07 02:23:25 -0600

    Add files via upload

--- src/embed/nogame.lua
+++ src/embed/nogame.lua
@@ -1,34 +1,64 @@
 --
--- Copyright (c) 2017 rxi
+-- Copyright (c) 2017 rxi, (c) 2019 ShoesForClues
 --
 -- This library is free software; you can redistribute it and/or modify it
 -- under the terms of the MIT license. See LICENSE for details.
 --
 
 function love.nogame()
+	function love.load()
+		love.graphics.setBackgroundColor(0,138,255)
+	end
 
-  function love.load()
-    love.graphics.setBackgroundColor(0, 138, 225)
-  end
+	function love.keypressed(key)
+		if key=="escape" then
+			love.event.quit()
+		end
+	end
 
-  function love.keypressed(key)
-    if key == "escape" then
-      os.exit()
-    end
-  end
+	local function drawText(str,y)
+		local screenw=love.graphics.getWidth()
+		local font=love.graphics.getFont()
+		love.graphics.print(str,(screenw-font:getWidth(str))/2,y)
+	end
+	
+	local radius=10
+	local d=1;
+	local r=0;
 
-  local function drawText(str, y)
-    local screenw = love.graphics.getWidth()
-    local font = love.graphics.getFont()
-    love.graphics.print(str, (screenw - font:getWidth(str)) / 2, y)
-  end
-
-  function love.draw()
-    love.graphics.setColor(255, 255, 255)
-    drawText("LoveDOS " .. love.getVersion(), 90)
-    drawText("No game", 102)
-    love.graphics.setColor(95, 181, 255)
-    drawText("Press ESCAPE to quit", 186)
-  end
-
+	function love.draw()
+		love.graphics.setColor(0,170,255)
+		for y=0,love.graphics.getHeight()/(radius*2) do
+			for x=0,love.graphics.getWidth()/(radius*2) do
+				if (y*radius*2)%(radius*4)==0 then
+					if (x*radius*2)%(radius*4)==0 then
+						love.graphics.circle("fill",x*radius*2+radius,y*radius*2+radius,r)
+					else
+						love.graphics.circle("fill",x*radius*2+radius,y*radius*2+radius,radius-r)
+					end
+				else
+					if (x*radius*2)%(radius*4)~=0 then
+						love.graphics.circle("fill",x*radius*2+radius,y*radius*2+radius,r)
+					else
+						love.graphics.circle("fill",x*radius*2+radius,y*radius*2+radius,radius-r)
+					end
+				end
+			end
+		end
+		love.graphics.setColor(255,255,255)
+		drawText("LoveDOS "..love.getVersion(),love.graphics.getHeight()*0.45)
+		drawText("No game",love.graphics.getHeight()*0.45+love.graphics.getFont():getHeight())
+		love.graphics.setColor(50,200,255)
+		drawText("Press ESCAPE to quit",love.graphics.getHeight()*0.85)
+	end
+	
+	function love.update()
+		r=r+d
+		if r>=radius then
+			d=-1
+		elseif r<=0 then
+			d=1
+		end
+		love.timer.sleep(0.05)
+	end
 end
